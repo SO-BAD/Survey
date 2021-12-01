8bd9cf59b2e7c59c16a361bce44af3ec7b709d30
hank <anima923@hotmail.com>
2021-12-01 23:25:55 +0800

    add 重作版面

--- include/add_survey.php
+++ include/add_survey.php
@@ -1,27 +1,23 @@
 <form action="" method="POST" class="form">
-    <div class="left">
-        title
-    </div>
-    <div class="right">
-        <input type="text">
+    <div class="top">
+        <input type="text" placeholder="請輸入主題">
     </div>
     <div class="content">
         <div class="ques">
             <div class="q_t">
-                <span>Question: <input type="text" class="q_t_i"name="q[0][]"></span>
+                <input type="text" class="q_t_i" name="q[0][]" placeholder="Input Question">
             </div>
-            <div class="q_o">
-                <div id="q_o0-1" class="q_o0-1">option:<input type="text" name="q[0][]"></div>
-                <div id="q_o0-2" class="q_o0-2">option:<input type="text" name="q[0][]"></div>
+            <div class="o_box">
+                <div id = "o0-1" class="o">
+                    <i class="far fa-circle"></i><input type="text" class="opt" name="q[0][]"placeholder="Option">
+                </div>
             </div>
-            <div class="q_add">
-                <i class="fas fa-plus" onclick="add_o(0)" style="cursor: pointer; ">&ensp;option</i>
-            </div>
+            <i class="fas fa-plus" onclick="add_o(0)" style="cursor: pointer; "></i>
         </div>
     </div>
     <div class="footer">
         <input type="button" value="add question" onclick="add_q()">
-        <input type="submit" value="送出">
+        <input type="submit" value="建立">
     </div>
 </form>
 <link rel="stylesheet" href="./css/add_survey.css">
--- js/add_survey.js
+++ js/add_survey.js
@@ -1,38 +1,27 @@
-var q_ct = 0;
-var q_o_ct = 2;
 function del(nu) {
     document.getElementById(nu).remove();
-    q_o_ct--;
 }
+var q_ct = 0;
+var opt_ct = [1];
 function add_o(num) {
-    if(q_o_ct <5){
-    q_o_ct++;
-    let x = "q_o" + num + "-";
-    let n = document.getElementById((x + "1")).cloneNode(true);
-    n.setAttribute("id", (x + q_o_ct));
-    let i = document.createElement("i");
-    i.setAttribute("class", "fas fa-plus");
-    i.setAttribute("onclick", ("del('" + x + q_o_ct + "');"));
-    n.appendChild(i);
-    let f = document.getElementsByClassName("q_o")[num];
-    f.appendChild(n);
-    }else{
-        alert("最多5個選項");
-    }
+    opt_ct[num]++;
+    let f = document.getElementsByClassName("o_box")[num];
+    let str = 'o' + num + '-' + opt_ct[num];
+    let str1 = "<div id = '" + str + "'  class='o'>";
+    let str2 = "<i class='far fa-circle'></i><input type='text' class='opt' name='q[0][]' placeholder='Option'>";
+    let str3 = "<i id = 'o" + num + "-" + opt_ct[num] + "a' class='fas fa-exclamation-triangle'></i><i class='fas fa-times'";
+    let str4 = 'onclick ="' + "del('" + str + "')" + '"' + "'></i>";
+    let d = document.createElement("div");
+    d.innerHTML = str1 + str2 + str3 + str4;
+    f.appendChild(d);
 }
 
-
-function add_q() {
-    if (q_ct < 1) {
-        q_ct++;
-        let clone = document.getElementsByClassName("ques")[0].cloneNode();
-        let html1 ="<div class='q_t'><span>Question: <input type='text' class='q_t_i'name='q[";
-        let html2 ="][]'></span></div><div class='q_o'><div id='q_o"+q_ct+"-1' class='q_o"+q_ct+"-1'>option:<input type='text' name='q["+q_ct+"][]'></div><div id='q_o"+q_ct+"-2' class='q_o"+q_ct+"-2'>option:<input type='text' name='q["+q_ct+"][]'></div></div><div class='q_add'><i class='fas fa-plus' onclick='add_o("+q_ct+")' style='cursor: pointer; '>&ensp;option</i></div>";
-        let f = document.getElementsByClassName("content")[0];
-        f.appendChild(clone);
-        clone.innerHTML = html1 + q_ct+html2;
-    } else {
-        alert("最多2題");
-    }
-
+function add_q(){
+    q_ct++;
+    opt_ct.push(1);
+    let f = document.getElementsByClassName("content")[0];
+    let c = document.getElementsByClassName("ques")[0].cloneNode(true);
+    c.getElementsByClassName("q_t_i")[0].setAttribute("name",("q["+q_ct+"][]"));
+    c.getElementsByClassName("fa-plus")[0].setAttribute("onclick",("add_o("+q_ct+")"));
+    f.appendChild(c);
 }
