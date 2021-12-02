750d46d76eda084e6cf2cbf3c8b0349cce193a8c
SO-BAD <sgws0279@gmail.com>
2021-12-02 13:26:22 +0800

    完成insert survey

--- api/survey_insert.php
+++ api/survey_insert.php
@@ -1,8 +1,24 @@
-<pre>
+
 <?php
+session_start();
+include "db.php";
+$acc_col = ['title','end_time','author'];
+$acc_data =[$_POST['s'][0],$_POST['s'][1],$_SESSION['account']['name']];
 
-print_r($_POST['s']);
-print_r($_POST['q']);
+insert('surveys',$acc_col,$acc_data);
 
+$sql = "SELECT `id` FROM `surveys` WHERE `author`='".$_SESSION['account']['name']."' ORDER BY `id` DESC LIMIT 0,1";
+
+
+$id = $pdo->query($sql)->fetchColumn();
+
+$opt_col = ['s_id','num','opt_num','opt'];
+
+foreach($_POST['q'] as $key => $data){
+    foreach ($data as $key1 => $value) {
+        $opt_data = [$id,$key,$key1,$value];
+        insert('opts',$opt_col,$opt_data);
+    }
+}
+header("location:../index.php");
 ?>
-</pre>
