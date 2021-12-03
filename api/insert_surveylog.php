ff8474e5a049bda7378ce1aa32d5c0e073aa2b01
SO-BAD <sgws0279@gmail.com>
2021-12-03 09:16:49 +0800

    修復增加opts表格count的BUG

--- api/insert_surveylog.php
+++ api/insert_surveylog.php
@@ -7,16 +7,14 @@
     if($ck_res>0){
         echo "<script>alert('已填寫過，無法再填寫');window.location.href='../index.php';</script>";
     }else{
-    echo $_POST['id']."<br>";
-    print_r($_POST['r']);
     $log_col= ['s_id','q_num','answer','u_id'];
     foreach ($_POST['r'] as $key =>$data){
         $log_data = [$_POST['id'],$key,$data[0],$_SESSION['account']['id']]; 
         insert('surveylog',$log_col,$log_data);
-        $sql = "SELECT `count` FROM `opts` WHERE `num` = '{$key}'AND `opt_num` = '{$data[0]}'";
+        $sql = "SELECT `count` FROM `opts` WHERE `s_id` = '{$_POST['id']}' AND `num` = '{$key}' AND `opt_num` = '{$data[0]}'";
         $count = $pdo->query($sql)->fetchColumn();
-        $count++;
-        $sql = "UPDATE `opts` SET `count`='{$count}'WHERE  `num` = '{$key}'AND `opt_num` = '{$data[0]}'";
+        $count = (int)$count+1;
+        $sql = "UPDATE `opts` SET `count`='{$count}' WHERE `s_id` = '{$_POST['id']}' AND `num` = '{$key}'AND `opt_num` = '{$data[0]}'";
         $pdo->exec($sql);
     }
     
